263b7191c31602ef82612755635fe4ee792df8f1
Yuki-biu <yuki199908@163.com>
2020-07-14 11:25:05 +0800

    flyBird-second

--- js/flyBird/js/bird.js
+++ js/flyBird/js/bird.js
@@ -1,7 +1,9 @@
 var bird = {
     flyTimer:null,  //控制小鸟飞翔的定时器
     wingTimer:null, //小鸟翅膀摆动的定时器
+    
     div: document.createElement('div'),
+    
     showBird: function(parentObj) {
         this.div.style.width = '40px'
         this.div.style.height = '48px'
@@ -13,5 +15,41 @@ var bird = {
         this.div.style.zIndex = '1'
         //将小鸟的div插入到游戏的界面当中
         parentObj.appendChild(this.div)
+    },
+
+    fallSpeed: 0,   //小鸟下落的速度
+    flyBird: function() {   //控制小鸟飞翔下落的函数
+        bird.flyTimer = setInterval(fly,50)
+        function fly() {
+            bird.div.style.top = bird.div.offsetTop + bird.fallSpeed++ + 'px'
+            if (bird.div.offsetTop >= 395){ //掉到地面时清除定时器
+                bird.fallSpeed = 0
+                clearInterval(bird.flyTimer)
+                clearInterval(bird.wingTimer)
+            }
+            if (bird.div.offsetTop < 0){ //控制小鸟不飞出屏幕
+                bird.div.style.top = 0
+                bird.fallSpeed = 1
+            }
+            if (bird.fallSpeed > 12) { //控制小鸟下落的速度最大为12
+                bird.fallSpeed = 12;
+            }
+        }
+    },
+
+    wingWave: function() {  //控制小鸟扇动翅膀
+        var up = ['url(img/up_bird0.png)', 'url(img/up_bird1.png)']
+        var down = ['url(img/down_bird0.png)', 'url(img/down_bird1.png)']
+        var i = 0, j = 0
+        bird.wingTimer = setInterval(wing, 120)
+        function wing() {
+            if (bird.fallSpeed > 0) {
+                bird.div.style.backgroundImage = down[i++]
+                if(i >= 2) {i = 0}
+            } else if (bird.fallSpeed < 0) {
+                bird.div.style.backgroundImage = up[j++]
+                if(j >= 2) {j = 0} 
+            }
+        }
     }
 }
