f8028d644c22bca37e22a99b1a551412b4d30817
Yuki-biu <yuki199908@163.com>
2020-07-20 14:31:40 +0800

    let

---
+++ es6/变量提升/let_const.js
@@ -0,0 +1,51 @@
+var obj = {}
+
+// console.log(name)
+
+// var name = "张三"
+
+//相当于
+// var name
+// name = "张三"
+
+// var name //使用var定义的变量，会将定义代码提到首部（变量提升）
+
+// console.log(name)
+// let name = "皮皮"
+
+// for(let i = 0; i < 10; i++){
+//     console.log(i)
+// }
+// console.log(i)
+
+// var a = []
+// var i
+// for(i = 0; i < 10; i++){
+//     a[i] = function() {
+//         console.log(i)
+//     }
+// }
+// a[6]()
+
+// for(let i = 0; i < 3; i++){ //父作用域
+//     let i = 'abc'   //子作用域
+//     console.log(i)  //与子作用域同级，打印abc
+// }
+
+//暂时性死区
+// var tmp = 123
+
+// if (true) {
+//     tmp = 'abc'
+//     let tmp
+// }
+
+var a = 'a'
+var a = 'aa'
+console.log(a)
+
+let b = 'b'
+let b = 'bb'
+console.log(b) //let不允许重复声明变量
+
+
