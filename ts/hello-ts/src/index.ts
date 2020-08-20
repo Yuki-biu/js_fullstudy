410501d92ca2739472119ca79b83adeb5af8d278
Yuki-biu <yuki199908@163.com>
2020-08-20 12:11:36 +0800

    ts

---
+++ ts/hello-ts/src/index.ts
@@ -0,0 +1,32 @@
+function greeter(person:string) {
+    return "hello, " + person
+}
+
+// greeter(1)
+const user = "wn"
+greeter(user)
+
+// 类型
+// let isLoading:boolean = false
+
+// let book: string = 'typeScript'
+
+// function warnUser():void {
+
+// }
+
+// let aa: undefined = undefined
+// let bb: void = undefined
+
+
+// let sym1: symbol = Symbol('key1')
+// let sym2: symbol = Symbol('key2')
+
+// BigInt
+const max = Number.MAX_SAFE_INTEGER
+
+const max1 = max
+const max2 = max
+max1 == max2 // true
+
+
