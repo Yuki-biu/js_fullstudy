2cf7efd2248e3c9064799b8d829de5b14b01d30c
Yuki-biu <yuki199908@163.com>
2020-08-26 10:08:49 +0800

    vue-router

---
+++ vue/vue-w/vue-routers/src/components/Hi.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="hello">
+      <h1>{{msg}}</h1>
+      <router-link :to="{name:'hi1', params:{name: 'hi111'}}">hi 1</router-link>
+      <span style="display:inline-block; width: 50px"></span>
+      <router-link to='/hi/hi2'>hi 2</router-link>
+    
+      <!-- 有组件引入就需要路由入口 -->
+      <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'hi',
+    data () {
+        return{
+            msg: 'Hi, I am wn'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue/vue-w/vue-routers/src/components/Hi2.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="hi">我是hi2页面{{$route.name}}</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
