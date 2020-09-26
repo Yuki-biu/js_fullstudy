9610e525c56182d145aa4512353cca58a5d69c77
Yuki-biu <yuki199908@163.com>
2020-09-26 22:35:45 +0800

    notebook

---
+++ vue/vue-notebook/note-book/controllers/mySqlConfig.js
@@ -0,0 +1,44 @@
+var mysql = require('mysql')
+var config = require('./defaultConfig')
+
+// 链接线程池
+var pool = mysql.createPool({
+    host: config.database.host,
+    user: config.database.USERNAME,
+    password: config.database.PASSWORD,
+    database: config.database.DATABASE,
+    port: config.database.PORT
+})
+
+// 统一连接数据库的方法
+let allServices = {
+    query: function(sql, values) {
+        return new Promise((resolve, reject) => {
+            pool.getConnection(function(err, connection) { // 连接数据库
+                if(err) {
+                    reject(err)
+                } else {
+                    connection.query(sql, values, (err, rows) => {
+                        if(err) {
+                            reject(err)
+                        } else {
+                            resolve(rows)
+                        }
+                        connection.release()    // 释放连接
+                    })
+                }
+            })
+        })
+    }
+}
+
+// 读取所有user表数据
+let getAllUsers = function() {
+    let _sql = `select * from users;`
+    return allServices.query(_sql)
+}
+
+// 导出方法
+module.exports = {
+    getAllUsers
+}
