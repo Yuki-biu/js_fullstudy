aeb3bdb2e89556bc698e4d2817afe4496308cb8f
Yuki-biu <yuki199908@163.com>
2020-09-28 23:45:29 +0800

    notebook

--- vue/vue-notebook/front/src/router/index.js
+++ vue/vue-notebook/front/src/router/index.js
@@ -1,22 +1,24 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'StarNotes',
+    component: () => import('@/views/StarNotes.vue'),
+    meta: {
+      title: '坛叽日记'
+    }
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/StarBanner',
+    name: 'StarBanner',
+    component: () => import('@/views/StarBanner.vue'),
+    meta: {
+      title: '欢迎'
+    }
   }
 ]
 
