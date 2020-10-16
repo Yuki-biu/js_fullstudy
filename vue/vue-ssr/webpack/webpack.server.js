34441c65cc3853278c4691cc620bd22118bff07f
Yuki-biu <yuki199908@163.com>
2020-10-16 14:42:19 +0800

    ssr

---
+++ vue/vue-ssr/webpack/webpack.server.js
@@ -0,0 +1,31 @@
+const path = require('path')
+const projectRoot = path.resolve(__dirname, '..')
+
+module.exports = {
+    target: 'node',
+    entry: path.join(projectRoot, 'src/entry-client.js'),
+    output: {
+        libraryYarget: 'commonjs2',
+        path: path.join(projectRoot, 'dist'),
+        filename: 'bundle.server.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                include: projectRoot,
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        alias: {
+            'vue$': 'vue/dist/vue.runtime.esm.js'
+        }
+    }
+}
