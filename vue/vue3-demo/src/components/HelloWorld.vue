88b59c3dda582e4bd7880ac9a74d90a9fef095dc
Yuki-biu <yuki199908@163.com>
2020-10-29 11:31:50 +0800

    vue3

---
+++ vue/vue3-demo/src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>{{state.count}} * 2 = {{double}}</h1>
+  <button @click="add">累加</button>
+</template>
+
+<script>
+import { computed, reactive } from 'vue'
+export default {
+  setup() {
+    const state = reactive({
+      count: 1
+    })
+    const num = ref(2)
+    function add() {
+      state.count++
+    }
+    const double = computed(() => state.count * 2)
+
+    return {
+      state,
+      add,
+      double,
+      num
+    }
+  }
+}
+</script>
